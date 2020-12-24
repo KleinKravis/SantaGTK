3fd45c493195d7552f53f9181661295353ea977e
Klein Kravis <37665485+KleinKravis@users.noreply.github.com>
2020-12-24 08:08:33 -0600

    Add script

---
+++ main.py
@@ -0,0 +1,17 @@
+import gi
+import webview
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+def on_activate(app):
+    win = Gtk.ApplicationWindow(application=app)
+    #show the page
+webview.create_window('SantaGTK', 'https://noradsanta.org')
+webview.start()
+
+# Create a new application
+app = Gtk.Application(application_id='com.github.hydromalis.SantaGTK')
+app.connect('activate', on_activate)
+
+# Run the application
+app.run(None)
