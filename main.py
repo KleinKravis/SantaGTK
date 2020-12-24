0a1f1c29ee37cb689c2f8cd582c4edb06b430286
Klein Kravis <37665485+KleinKravis@users.noreply.github.com>
2020-12-24 10:12:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def on_activate(app):
 webview.create_window('SantaGTK', 'https://noradsanta.org')
 webview.start()
 
+#kill the app when x is pressed
+def on_window1_destroy(self, *args):
+    Gtk.main_quit()
+
 # Create a new application
 app = Gtk.Application(application_id='com.github.hydromalis.SantaGTK')
 app.connect('activate', on_activate)
