cd24bac0bafb5719d59f2416e5eb581cb95c35e4
Klein Kravis <37665485+KleinKravis@users.noreply.github.com>
2020-12-24 12:30:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "You may have to enter your password, don't leave"
-read -p "Press enter to begin"
 echo "Installing"
-mkdir ~/.local/bin/santagtk
-cp santagtk.py ~/.local/bin/santagtk
-cp icon.png ~/.local/bin/santagtk
-sudo cp santagtk.desktop /usr/share/applications
+cd SantaGTK
+mkdir .local/bin/santagtk
+cp santagtk.py .local/bin/santagtk
+cp icon.png .local/bin/santagtk
+cp santagtk.desktop /usr/share/applications
 clear
 echo "SantaGTK has been installed. Merry Christmas!"
